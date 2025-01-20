731620d6a3710c3dd60584a8aeeb8f2d50d590da
yunjeongleekr <yunjeongleekr@gmail.com>
2025-01-20 21:56:22 +0900

    #3.3

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+import Circle from "./Circle";
+
+function App() {
+  return (
+    <div>
+      <Circle bgColor="teal" />
+      <Circle bgColor="tomato" />
+    </div>
+  );
+}
+
+export default App;
