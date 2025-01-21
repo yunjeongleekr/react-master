3f310cdc001ab483572ce051a7afabf08ade6bdf
yunjeongleekr <yunjeongleekr@gmail.com>
2025-01-21 22:24:20 +0900

    #3.6

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,17 @@
-import { useState } from "react";
-// import styled from "styled-components";
+import styled from "styled-components";
+
+const Container = styled.div`
+  bacground-color: ${(props) => props.theme.bgColor};
+`;
+const H1 = styled.h1`
+  color: ${(props) => props.theme.textColor};
+`;
 
 function App() {
-  const [value, setValue] = useState("");
-  const onChange = (event: React.FormEvent<HTMLInputElement>) => {
-    console.log(event.currentTarget.value);
-    const {
-      currentTarget: { value },
-    } = event;
-    setValue(value);
-  };
-  const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
-    event.preventDefault();
-    console.log("hello", value);
-  };
   return (
-    <div>
-      <form onSubmit={onSubmit}>
-        <input
-          value={value}
-          onChange={onChange}
-          type="text"
-          placeholder="username"
-        />
-        <button>Log in</button>
-      </form>
-    </div>
+    <Container>
+      <H1>Protected</H1>
+    </Container>
   );
 }
 
