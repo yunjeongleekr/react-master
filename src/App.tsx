275ea72cf38761140084991e785d98286ac97476
yunjeongleekr <yunjeongleekr@gmail.com>
2025-01-21 22:08:04 +0900

    #3.5

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,30 @@
-import styled from "styled-components";
-import Circle from "./Circle";
+import { useState } from "react";
+// import styled from "styled-components";
 
 function App() {
+  const [value, setValue] = useState("");
+  const onChange = (event: React.FormEvent<HTMLInputElement>) => {
+    console.log(event.currentTarget.value);
+    const {
+      currentTarget: { value },
+    } = event;
+    setValue(value);
+  };
+  const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    console.log("hello", value);
+  };
   return (
     <div>
-      <Circle borderColor="white" bgColor="teal" />
-      <Circle text="im here" bgColor="tomato" />
+      <form onSubmit={onSubmit}>
+        <input
+          value={value}
+          onChange={onChange}
+          type="text"
+          placeholder="username"
+        />
+        <button>Log in</button>
+      </form>
     </div>
   );
 }
